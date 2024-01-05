0519dc386f08dc3afb5b30b136869ac5d4894e30
Daffa <daffamfzn@gmail.com>
2024-01-05 21:52:44 +0700

    fix: changed field of author

--- models/author.go
+++ models/author.go
@@ -1,6 +1,7 @@
 package models
 
 type Author struct {
-	Name string `json:"name" binding:"required,max=50"`
-	Age  int    `json:"age" binding:"required,gte=1,lte=150"`
+	Name        string `json:"name" binding:"required,max=50"`
+	Institution string `json:"institution" binding:"max=60"`
+	Occupation  string `json:"occupation" binding:"gte=1,lte=150"`
 }
