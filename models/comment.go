62b5c97c659feac1c8ea4fcd36d8742fe91c1c0b
Daffa <daffamfzn@gmail.com>
2024-01-05 21:51:14 +0700

    fix: changed field of comment and post

--- models/comment.go
+++ models/comment.go
@@ -4,6 +4,6 @@ type Comment struct {
 	ID      string `json:"id" binding:"required"`
 	Content string `json:"content" binding:"required,max=300"`
 	Time    string `json:"time" binding:"required"`
+	PostID  string `json:"postid" binding:"required"`
 	Author  Author `json:"author" binding:"required"`
-	Post    Post   `json:"post" binding:"required"`
 }
--- models/post.go
+++ models/post.go
@@ -1,9 +1,10 @@
 package models
 
 type Post struct {
-	ID      string `json:"id" binding:"required"`
-	Title   string `json:"title" binding:"required,max=60"`
-	Content string `json:"content" binding:"required,max=1000"`
-	Time    string `json:"time" binding:"required"`
-	Author  Author `json:"author" binding:"required"`
+	ID       string    `json:"id" binding:"required"`
+	Title    string    `json:"title" binding:"required,max=60"`
+	Content  string    `json:"content" binding:"required,max=1000"`
+	Time     string    `json:"time" binding:"required"`
+	Author   Author    `json:"author" binding:"required"`
+	Comments []Comment `json:"comments"`
 }
