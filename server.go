ff457b30795e99d60f3addaa7925ccedace129c8
Daffa <daffamfzn@gmail.com>
2024-01-05 15:58:20 +0700

    feat: added initial folder structure

--- server.go
+++ server.go
@@ -7,5 +7,11 @@ import (
 func main() {
 	server := gin.Default()
 
+	apiRoutes := server.Group("/api")
+	{
+		apiRoutes.GET("/posts", func(c *gin.Context) {
+		})
+	}
+
 	server.Run("localhost:8080")
 }
