6f7589ab95185fa9f4793025e8298caface5f36e
Daffa <daffamfzn@gmail.com>
2024-01-05 23:13:59 +0700

    fix: changed field name

--- models/comment.go
+++ models/comment.go
@@ -1,9 +1,9 @@
 package models
 
 type Comment struct {
-	ID      string `json:"id" binding:"required"`
-	Content string `json:"content" binding:"required,max=300"`
-	Time    string `json:"time" binding:"required"`
-	PostID  string `json:"postid" binding:"required"`
-	Author  Author `json:"author" binding:"required"`
+	ID            string `json:"id" binding:"required"`
+	Content       string `json:"content" binding:"required,max=300"`
+	Time          string `json:"time" binding:"required"`
+	PostID        string `json:"postId" binding:"required"`
+	CommentAuthor Author `json:"commentAuthor" binding:"required"`
 }
