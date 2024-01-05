52f8547e0d3d3d2620763527e15b8684ba590caf
Daffa <daffamfzn@gmail.com>
2024-01-05 23:15:40 +0700

    fix: removed unused field

--- models/comment.go
+++ models/comment.go
@@ -4,6 +4,5 @@ type Comment struct {
 	ID            string `json:"id" binding:"required"`
 	Content       string `json:"content" binding:"required,max=300"`
 	Time          string `json:"time" binding:"required"`
-	PostID        string `json:"postId" binding:"required"`
 	CommentAuthor Author `json:"commentAuthor" binding:"required"`
 }
