1475910dfdf77232c1fc23611cb6550364aa4cb6
Daffa <daffamfzn@gmail.com>
2024-01-06 23:29:49 +0700

    feat: separated date and time field

--- models/comment.go
+++ models/comment.go
@@ -3,6 +3,7 @@ package models
 type Comment struct {
 	ID            string `json:"id" binding:"required"`
 	Content       string `json:"content" binding:"required,max=300"`
+	Date          string `json:"date" binding:"required"`
 	Time          string `json:"time" binding:"required"`
 	CommentAuthor Author `json:"commentAuthor" binding:"required"`
 }
--- models/post.go
+++ models/post.go
@@ -4,6 +4,7 @@ type Post struct {
 	ID         string    `json:"id" binding:"required"`
 	Title      string    `json:"title" binding:"required,max=60"`
 	Content    string    `json:"content" binding:"required,max=1000"`
+	Date       string    `json:"date" binding:"required"`
 	Time       string    `json:"time" binding:"required"`
 	PostAuthor Author    `json:"postAuthor" binding:"required"`
 	Comments   []Comment `json:"comments"`
