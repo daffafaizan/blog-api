c29675017428abb2cd2af3668e9e63d815111ea5
Daffa <daffamfzn@gmail.com>
2024-01-07 17:57:25 +0700

    feat: added bson to model fields

--- models/post.go
+++ models/post.go
@@ -1,11 +1,11 @@
 package models
 
 type Post struct {
-	ID         string    `json:"id" binding:"required"`
-	Title      string    `json:"title" binding:"required,max=60"`
-	Content    string    `json:"content" binding:"required,max=1000"`
-	Date       string    `json:"date" binding:"required"`
-	Time       string    `json:"time" binding:"required"`
-	PostAuthor Author    `json:"postAuthor" binding:"required"`
-	Comments   []Comment `json:"comments"`
+	ID         string    `json:"id" bson:"id" binding:"required"`
+	Title      string    `json:"title" bson:"title" binding:"required,max=60"`
+	Content    string    `json:"content" bson:"content" binding:"required,max=1000"`
+	Date       string    `json:"date" bson:"date" binding:"required"`
+	Time       string    `json:"time" bson:"time" binding:"required"`
+	PostAuthor Author    `json:"postAuthor" bson:"postAuthor" binding:"required"`
+	Comments   []Comment `json:"comments" bson:"comments"`
 }
