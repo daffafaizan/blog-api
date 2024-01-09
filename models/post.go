c2501bb5ff8c3e08be2524e691d0e7757edeae36
Daffa <daffamfzn@gmail.com>
2024-01-09 17:54:35 +0700

    feat: added and modified post field

--- models/post.go
+++ models/post.go
@@ -4,7 +4,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Post struct {
 	Title      string             `json:"title" bson:"title" binding:"required,max=60"`
-	Content    string             `json:"content" bson:"content" binding:"required,max=1000"`
+	Summary    string             `json:"summary" bson:"summary" binding:"required,max=300"`
+	Content    string             `json:"content" bson:"content" binding:"required"`
 	Date       string             `json:"date" bson:"date" binding:"required"`
 	Time       string             `json:"time" bson:"time" binding:"required"`
 	PostAuthor Author             `json:"postAuthor" bson:"postAuthor" binding:"required"`
